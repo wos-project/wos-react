23148a2b305925c8b3fb164771263804de2d91b6
questorihub <support@questori.com>
2022-03-21 18:21:16 -0400

    readme

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,28 @@ import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { useState, useEffect }  from 'react';
 
 export default function Home() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [contract, setContract] = useState(null);
+
+  useEffect(() => {
+        fetch("https://worldos.cloud/v1/contract/0xBdd3D3e5b291E6Fe950503c666b0CCe32Abf8804")
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    setIsLoaded(true);
+                    setContract(data);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+      }, [])
+
   return <div>
       <div>
         <Box
